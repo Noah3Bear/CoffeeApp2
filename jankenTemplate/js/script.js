5d36858b03a5748511e74e63124c246881a89faf
Noah3Bear <94768130+Noah3Bear@users.noreply.github.com>
2021-11-21 09:36:44 +0900

    Add files via upload

---
+++ jankenTemplate/js/script.js
@@ -0,0 +1,39 @@
+$('.fruty').on('click', function(){
+
+    $('.result').html('<div>あなたにおススメはケニアです</div><img src="https://coffeemecca.jp/wp-content/uploads/2015/10/Flag-of-Kenya-coffee.jpg" class="coffee-img" alt="">');
+
+
+});
+$('.fresh').on('click', function(){
+
+    $('.result').html('<div>あなたにおススメはエチオピアです</div><img src="https://coffeemecca.jp/wp-content/uploads/2015/10/Flag-of-Ethiopia-coffee.jpg" class="coffee-img" alt="">'
+        );
+    
+
+
+});
+$('.sweet').on('click', function(){
+
+    $('.result').html('<div>あなたにおススメはコロンビアです</div><img src="https://coffeemecca.jp/wp-content/uploads/2015/10/Flag-of-Colombia-coffee.jpg" class="coffee-img" alt="">'
+        );
+
+
+});
+$('.deep').on('click', function(){
+
+    $('.result').html('<div>あなたにおススメはブラジルです</div><img src="https://coffeemecca.jp/wp-content/uploads/2015/10/Flag-of-Brazil-coffee.jpg" class="coffee-img" alt="">'
+        );
+
+
+});
+$('.bitter').on('click', function(){
+
+    $('.result').html('<div>あなたにおススメはインドネシアです</div><img src="https://coffeemecca.jp/wp-content/uploads/2015/10/Flag-of-Indonesia-coffee.jpg" class="coffee-img" alt="">'
+        );
+
+
+});
+
+
+// alert('おはようございます.コーヒー選びで悩んでませんか？');
+
